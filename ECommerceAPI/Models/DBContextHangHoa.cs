fe4e81703bb7d0c52b1b3c564de10f9f59cc53bc
nghia.nguyen <nghia.nguyen@s3corp.com.vn>
2024-09-24 17:31:47 +0700

    firts
    add

---
+++ ECommerceAPI/Models/DBContextHangHoa.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Models
+{
+    public class DBContextHangHoa : DbContext
+    {
+        public DBContextHangHoa(DbContextOptions<DBContextHangHoa> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
