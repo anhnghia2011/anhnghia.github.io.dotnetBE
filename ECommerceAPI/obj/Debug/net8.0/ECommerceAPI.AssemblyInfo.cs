ceeaa6539f2fbc87b2346771f453411424bbf5c7
nghia.nguyen <nghia.nguyen@s3corp.com.vn>
2024-10-09 17:13:46 +0700

    cm

--- ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488b3174131e0a362abff948b66e6258e41f4fdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26d7bee37553f51eecc6e18edd0223beacc67bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
