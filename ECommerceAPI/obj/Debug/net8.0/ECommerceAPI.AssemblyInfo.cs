dde67e03c3721562f8f58cd86234140b84779dd1
anhnghia2011 <nguyenanhnghia81@81gmail.com>
2024-10-22 00:22:15 +0700

    cm22/10

--- ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfea3132ba8d077979a2a01cef49b15ee5c9e7e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c729ab8b5e6d7e305bb0c3ded2d091aeca6497fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
